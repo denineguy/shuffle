b410e7b1465d8f82a664522f6ebf27b691df00c8
Denine Guy <denineguy@gmail.com>
2014-09-02 22:22:15 -0400

    added delete_at method and made sure to not push in nil value)

--- shuffle.rb
+++ shuffle.rb
@@ -3,7 +3,8 @@ def shuffle
   new_array = []
   until array.empty?
     i = rand(0..array.length)
-    new_array << array[i]
-    array[i].delete 
+    new_array << array[i] if array[i] != nil
+    array.delete_at(i)
   end
+  new_array
 end
